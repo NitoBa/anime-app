dddd8777e8efa48744929beb41706b2c417873c7
Bruno Alves <bruno@sthorm.io>
2022-08-28 19:37:11 -0300

    feat: add list release

---
+++ src/components/ListRelease/index.tsx
@@ -0,0 +1,24 @@
+export function ListRelease() {
+  return (
+    <div className="flex flex-col gap-[2.4rem]">
+      <h4 className="font-semibold text-[3.6rem]">New Realease</h4>
+      <div className="flex gap-[3rem] items-center justify-center w-full overflow-x-scroll">
+        {new Array(14).fill(0).map((_) => (
+          <div className="h-[28.4rem] w-[17rem] flex flex-col gap-[.8rem] items-center flex-shrink-0">
+            <div className="h-[25.3rem] w-full relative flex flex-col items-center">
+              <img
+                src="https://picsum.photos/300"
+                alt=""
+                className="rounded-xl w-full h-full"
+              />
+              <div className="flex flex-col items-center justify-end bg-gradient-to-b from-transparent to-[#0F1E29] absolute h-full w-full rounded-xl pb-[1.2rem]">
+                <span className="">Episode 1132</span>
+              </div>
+            </div>
+            <strong className="font-semibold text-center">One Piece</strong>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import { ExplorerSection } from "../components/ExplorerSection";
+import { ListRelease } from "../components/ListRelease";
 
 export function HomePage() {
   return (
-    <div className="flex flex-col mt-[4rem] max-w-[117rem] mx-auto">
+    <div className="flex flex-col mt-[4rem] gap-[4rem] max-w-[117rem] mx-auto">
       <ExplorerSection />
+      <ListRelease />
     </div>
   );
 }
