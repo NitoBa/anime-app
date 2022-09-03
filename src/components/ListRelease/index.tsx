ce556c78771d7e016690bdf10dea142f66ef9e56
Bruno Alves <bruno@sthorm.io>
2022-09-03 18:53:04 -0300

    refactor: create recent episodie card

--- src/components/ListRelease/index.tsx
+++ src/components/ListRelease/index.tsx
@@ -1,6 +1,7 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { Link } from "react-router-dom";
 import { useSearchRecentEpisodes } from "../../hooks/useSearchRecentEpisodes";
+import { RecentEpisodeCard } from "../RecentEpisodieCard";
 export function ListRelease() {
   const { data: recentEpisodes, isLoading } = useSearchRecentEpisodes();
 
@@ -26,21 +27,7 @@ export function ListRelease() {
               className="h-[28.4rem] w-[17rem] flex flex-col gap-[.8rem] items-center flex-shrink-0 transition-all duration-300 hover:brightness-110 cursor-pointer"
             >
               <Link to={`/watch/${episode.episodeId}`}>
-                <div className="h-[25.3rem] w-full relative flex flex-col items-center">
-                  <img
-                    src={episode.image}
-                    alt=""
-                    className="rounded-xl w-full h-full object-cover"
-                  />
-                  <div className="flex flex-col items-center justify-end bg-gradient-to-b from-transparent to-[#0F1E29] absolute h-full w-full rounded-xl pb-[1.2rem]">
-                    <span className="">Episode {episode.episodeNumber}</span>
-                  </div>
-                </div>
-                <strong className="font-semibold text-center">
-                  {episode?.title.romaji ??
-                    episode?.title.english ??
-                    episode?.title.native}
-                </strong>
+                <RecentEpisodeCard episode={episode} />
               </Link>
             </SplideSlide>
           ))}
---
+++ src/components/RecentEpisodieCard/index.tsx
@@ -0,0 +1,27 @@
+import { RecentEpisode } from "../../types/recentEpisode";
+
+type Props = {
+  episode: RecentEpisode;
+};
+
+export function RecentEpisodeCard({ episode }: Props) {
+  return (
+    <>
+      <div className="h-[25.3rem] w-full relative flex flex-col items-center">
+        <img
+          src={episode.image}
+          alt=""
+          className="rounded-xl w-full h-full object-cover"
+        />
+        <div className="flex flex-col items-center justify-end bg-gradient-to-b from-transparent to-[#0F1E29] absolute h-full w-full rounded-xl pb-[1.2rem]">
+          <span className="">Episode {episode.episodeNumber}</span>
+        </div>
+      </div>
+      <strong className="font-semibold text-center">
+        {episode?.title.romaji ??
+          episode?.title.english ??
+          episode?.title.native}
+      </strong>
+    </>
+  );
+}
